789d40632e3bb80910895ca979a33aaa98b2c156
dan-durkin <daniel.h.durkin@gmail.com>
2016-03-20 22:45:49 -0400

    Progress on mobile optimization

--- dist/scripts/login.js
+++ dist/scripts/login.js
@@ -5,11 +5,11 @@ TwentyFour.login = (function (){
 	var ref = TwentyFour.data.getRef();
 	var logged_in = false;
 	var authData;
-	
+
 	/***
 	Private Methods
 	***/
-	
+
 	ref.onAuth(function (data) {
 		if (data) {
 			logged_in = true;
@@ -23,7 +23,7 @@ TwentyFour.login = (function (){
 			}
 		}
 	});
-	
+
 	function authHandler(error, data) {
 		if (error) {
 			console.log("Login Failed!", error);
@@ -34,15 +34,15 @@ TwentyFour.login = (function (){
 			console.log("Authenticated successfully with payload:", authData);
 		}
 	}
-	
+
 	function populateData(){
 		var welcomeContainer = $('.welcome');
 		welcomeContainer.empty();
-		
+
 		if(logged_in){
 			var username = authData.facebook.displayName;
 			var profPicUrl = authData.facebook.profileImageURL;
-			
+
 			var welcomeElement = $("<p>Welcome back, " + username + "!</p>");
 			welcomeContainer.append(welcomeElement);
 			TwentyFour.display.loginDisplay(logged_in);
@@ -52,32 +52,32 @@ TwentyFour.login = (function (){
 			TwentyFour.display.loginDisplay(logged_in);
 		}
 	}
-	
+
 	/***
 	Public Methods
 	***/
-	
+
 	function getAuthData () {
 		return authData;
 	}
-	
+
 	function facebookLogin () {
 		ref.authWithOAuthPopup("facebook", authHandler,{
 		  remember: "sessionOnly",
 		  scope: "public_profile"
 		});
 	}
-	
+
 	function logout () {
-		ref.unauth();	
+		ref.unauth();
 		location.reload();
 	}
-	
+
 	function config (){
 		if(localStorage.getItem("user")){
 			var user = JSON.parse(localStorage.getItem("user"));
 			var expiration = new Date(user.expires * 1000).getTime() - new Date().getTime()
-			
+
 			if(expiration <= 0 ){
 				localStorage.clear();
 				logout();
@@ -92,7 +92,7 @@ TwentyFour.login = (function (){
 		else{
 			logged_in = false;
 		}
-		
+
 		populateData();
 	}
 
@@ -103,29 +103,3 @@ TwentyFour.login = (function (){
 		getAuthData:getAuthData
 	};
 }());
-
-	/***
-	create config function to run on setup
-	check localstorage on page load
-	
-	new Date(user.expires * 1000).getTime() - new Date().getTime() <= 0
-false
-
-	// On Page Load
-	// 1. Pull in data from localStorage -- localStorage.getItem("whateverYoUCallthis") / localStorage.setItem("whatever", value)
-	// Note: value has to be a string (JSON.stringify(value))
-	// Note: JSON.parse to turn it into an object.  Check to make sure that it actually has a value before you try to JSON.parse
-	// 2. Check expiration of authData.expires
-	// 3. If it is expired, clear localStorage, keep logged_in as false
-	// 4. If it is not expired
-	// 4a. Make sure that authData variable has been set
-	// 4b. Set logged_in = true
-	// 4c. Call a function to render the user's information
-	
-	// If they are not logged in and they click the auth button:
-	// 1. Execute oAuthPopup
-	// 2. On success, set authData variable
-	// 3. Set logged_in = true
-	// 4. Set localStorage
-	// 5. Call a function to render the user's information
-***/
